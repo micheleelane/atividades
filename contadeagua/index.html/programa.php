84266b00e1e428a0fb902f2d0801b2bf1403c76b
micheleelane <miih.dance@hotmail.com>
2013-12-18 16:18:52 -0200

    Create programa.php

---
+++ contadeagua/index.html/programa.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Calculadora de Michele</title>
+</head>
+
+<body>
+<form action="programa.php" method="post">
+	<fieldset>
+	<h3>calculadora de água</h3>
+	<h4>Por favor,digite os valores das contas</h4>
+
+	<tabel>Primeiro mes</label> &nbsp;<input type="text" name="m1" /> <br/>
+	<tabel>Segundo mes</label> &nbsp;<input type="text" name="m2" /> <br/>
+	<tabel>Terceiro mes</label> &nbsp;<input type="text" name="m3" /> <br/> <br/>
+	</fieldset>	
+	<input type="submit" value="Enviar" />
+	</form>
+</body>
+</html>
